85ef22f7f0506ef0c7d86a7f7fca48cf61b75eb1
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-09-18 18:06:20 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,296 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class CalcForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(CalcForm))
+        Key7 = New Button()
+        Key8 = New Button()
+        Key9 = New Button()
+        KeyDiv = New Button()
+        KeyMul = New Button()
+        KeyLess = New Button()
+        KeyPlus = New Button()
+        Key6 = New Button()
+        Key5 = New Button()
+        Key4 = New Button()
+        Key3 = New Button()
+        Key2 = New Button()
+        Key1 = New Button()
+        KeyComma = New Button()
+        Key0 = New Button()
+        KeyReverse = New Button()
+        KeyCalc = New Button()
+        KeyPercent = New Button()
+        KeyPartClear = New Button()
+        KeyClearAll = New Button()
+        ResultField = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Key7
+        ' 
+        Key7.Location = New Point(10, 144)
+        Key7.Name = "Key7"
+        Key7.Size = New Size(75, 57)
+        Key7.TabIndex = 0
+        Key7.Text = "7"
+        Key7.UseVisualStyleBackColor = True
+        ' 
+        ' Key8
+        ' 
+        Key8.Location = New Point(91, 144)
+        Key8.Name = "Key8"
+        Key8.Size = New Size(75, 57)
+        Key8.TabIndex = 1
+        Key8.Text = "8"
+        Key8.UseVisualStyleBackColor = True
+        ' 
+        ' Key9
+        ' 
+        Key9.Location = New Point(172, 144)
+        Key9.Name = "Key9"
+        Key9.Size = New Size(75, 57)
+        Key9.TabIndex = 2
+        Key9.Text = "9"
+        Key9.UseVisualStyleBackColor = True
+        ' 
+        ' KeyDiv
+        ' 
+        KeyDiv.Location = New Point(253, 81)
+        KeyDiv.Name = "KeyDiv"
+        KeyDiv.Size = New Size(75, 57)
+        KeyDiv.TabIndex = 3
+        KeyDiv.Text = "/"
+        KeyDiv.UseVisualStyleBackColor = True
+        ' 
+        ' KeyMul
+        ' 
+        KeyMul.BackColor = SystemColors.Control
+        KeyMul.Location = New Point(253, 144)
+        KeyMul.Name = "KeyMul"
+        KeyMul.Size = New Size(75, 57)
+        KeyMul.TabIndex = 4
+        KeyMul.Text = "X"
+        KeyMul.UseVisualStyleBackColor = False
+        ' 
+        ' KeyLess
+        ' 
+        KeyLess.BackColor = SystemColors.Control
+        KeyLess.Location = New Point(253, 207)
+        KeyLess.Name = "KeyLess"
+        KeyLess.Size = New Size(75, 57)
+        KeyLess.TabIndex = 5
+        KeyLess.Text = "-"
+        KeyLess.UseVisualStyleBackColor = False
+        ' 
+        ' KeyPlus
+        ' 
+        KeyPlus.BackColor = SystemColors.Control
+        KeyPlus.Location = New Point(253, 270)
+        KeyPlus.Name = "KeyPlus"
+        KeyPlus.Size = New Size(75, 57)
+        KeyPlus.TabIndex = 6
+        KeyPlus.Text = "+"
+        KeyPlus.UseVisualStyleBackColor = False
+        ' 
+        ' Key6
+        ' 
+        Key6.Location = New Point(172, 207)
+        Key6.Name = "Key6"
+        Key6.Size = New Size(75, 57)
+        Key6.TabIndex = 9
+        Key6.Text = "6"
+        Key6.UseVisualStyleBackColor = True
+        ' 
+        ' Key5
+        ' 
+        Key5.Location = New Point(91, 207)
+        Key5.Name = "Key5"
+        Key5.Size = New Size(75, 57)
+        Key5.TabIndex = 8
+        Key5.Text = "5"
+        Key5.UseVisualStyleBackColor = True
+        ' 
+        ' Key4
+        ' 
+        Key4.Location = New Point(10, 207)
+        Key4.Name = "Key4"
+        Key4.Size = New Size(75, 57)
+        Key4.TabIndex = 7
+        Key4.Text = "4"
+        Key4.UseVisualStyleBackColor = True
+        ' 
+        ' Key3
+        ' 
+        Key3.Location = New Point(172, 270)
+        Key3.Name = "Key3"
+        Key3.Size = New Size(75, 57)
+        Key3.TabIndex = 12
+        Key3.Text = "3"
+        Key3.UseVisualStyleBackColor = True
+        ' 
+        ' Key2
+        ' 
+        Key2.Location = New Point(91, 270)
+        Key2.Name = "Key2"
+        Key2.Size = New Size(75, 57)
+        Key2.TabIndex = 11
+        Key2.Text = "2"
+        Key2.UseVisualStyleBackColor = True
+        ' 
+        ' Key1
+        ' 
+        Key1.Location = New Point(10, 270)
+        Key1.Name = "Key1"
+        Key1.Size = New Size(75, 57)
+        Key1.TabIndex = 10
+        Key1.Text = "1"
+        Key1.UseVisualStyleBackColor = True
+        ' 
+        ' KeyComma
+        ' 
+        KeyComma.Location = New Point(172, 333)
+        KeyComma.Name = "KeyComma"
+        KeyComma.Size = New Size(75, 57)
+        KeyComma.TabIndex = 15
+        KeyComma.Text = ","
+        KeyComma.UseVisualStyleBackColor = True
+        ' 
+        ' Key0
+        ' 
+        Key0.Location = New Point(91, 333)
+        Key0.Name = "Key0"
+        Key0.Size = New Size(75, 57)
+        Key0.TabIndex = 14
+        Key0.Text = "0"
+        Key0.UseVisualStyleBackColor = True
+        ' 
+        ' KeyReverse
+        ' 
+        KeyReverse.Location = New Point(10, 333)
+        KeyReverse.Name = "KeyReverse"
+        KeyReverse.Size = New Size(75, 57)
+        KeyReverse.TabIndex = 13
+        KeyReverse.Text = "+/-"
+        KeyReverse.UseVisualStyleBackColor = True
+        ' 
+        ' KeyCalc
+        ' 
+        KeyCalc.Location = New Point(253, 333)
+        KeyCalc.Name = "KeyCalc"
+        KeyCalc.Size = New Size(75, 57)
+        KeyCalc.TabIndex = 16
+        KeyCalc.Text = "="
+        KeyCalc.UseVisualStyleBackColor = True
+        ' 
+        ' KeyPercent
+        ' 
+        KeyPercent.Location = New Point(10, 81)
+        KeyPercent.Name = "KeyPercent"
+        KeyPercent.Size = New Size(75, 57)
+        KeyPercent.TabIndex = 17
+        KeyPercent.Text = "%"
+        KeyPercent.UseVisualStyleBackColor = True
+        ' 
+        ' KeyPartClear
+        ' 
+        KeyPartClear.Location = New Point(91, 81)
+        KeyPartClear.Name = "KeyPartClear"
+        KeyPartClear.Size = New Size(75, 57)
+        KeyPartClear.TabIndex = 18
+        KeyPartClear.Text = "CE"
+        KeyPartClear.UseVisualStyleBackColor = True
+        ' 
+        ' KeyClearAll
+        ' 
+        KeyClearAll.Location = New Point(172, 81)
+        KeyClearAll.Name = "KeyClearAll"
+        KeyClearAll.Size = New Size(75, 57)
+        KeyClearAll.TabIndex = 19
+        KeyClearAll.Text = "C"
+        KeyClearAll.UseVisualStyleBackColor = True
+        ' 
+        ' ResultField
+        ' 
+        ResultField.BackColor = SystemColors.Control
+        ResultField.BorderStyle = BorderStyle.None
+        ResultField.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        ResultField.Location = New Point(12, 49)
+        ResultField.Name = "ResultField"
+        ResultField.Size = New Size(316, 26)
+        ResultField.TabIndex = 20
+        ResultField.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' CalcForm
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(344, 398)
+        Controls.Add(ResultField)
+        Controls.Add(KeyClearAll)
+        Controls.Add(KeyPartClear)
+        Controls.Add(KeyPercent)
+        Controls.Add(KeyCalc)
+        Controls.Add(KeyComma)
+        Controls.Add(Key0)
+        Controls.Add(KeyReverse)
+        Controls.Add(Key3)
+        Controls.Add(Key2)
+        Controls.Add(Key1)
+        Controls.Add(Key6)
+        Controls.Add(Key5)
+        Controls.Add(Key4)
+        Controls.Add(KeyPlus)
+        Controls.Add(KeyLess)
+        Controls.Add(KeyMul)
+        Controls.Add(KeyDiv)
+        Controls.Add(Key9)
+        Controls.Add(Key8)
+        Controls.Add(Key7)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "CalcForm"
+        Text = "Calculatrice"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Key7 As Button
+    Friend WithEvents Key8 As Button
+    Friend WithEvents Key9 As Button
+    Friend WithEvents KeyDiv As Button
+    Friend WithEvents KeyMul As Button
+    Friend WithEvents KeyLess As Button
+    Friend WithEvents KeyPlus As Button
+    Friend WithEvents Key6 As Button
+    Friend WithEvents Key5 As Button
+    Friend WithEvents Key4 As Button
+    Friend WithEvents Key3 As Button
+    Friend WithEvents Key2 As Button
+    Friend WithEvents Key1 As Button
+    Friend WithEvents KeyComma As Button
+    Friend WithEvents Key0 As Button
+    Friend WithEvents KeyReverse As Button
+    Friend WithEvents KeyCalc As Button
+    Friend WithEvents KeyPercent As Button
+    Friend WithEvents KeyPartClear As Button
+    Friend WithEvents KeyClearAll As Button
+    Friend WithEvents ResultField As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,200 @@
+﻿Imports System.Linq.Expressions
+Imports System.Text.RegularExpressions
+
+Public Class CalcForm
+    Private Sub KeyPercent_Click(sender As Object, e As EventArgs) Handles KeyPercent.Click
+        Me.HandlekeyInput("%")
+    End Sub
+
+    Private Sub KeyPartClear_Click(sender As Object, e As EventArgs) Handles KeyPartClear.Click
+        Me.ResetTextBox()
+    End Sub
+
+    Private Sub KeyClearAll_Click(sender As Object, e As EventArgs) Handles KeyClearAll.Click
+        Me.ResetTextBox()
+    End Sub
+
+    Private Sub KeyDiv_Click(sender As Object, e As EventArgs) Handles KeyDiv.Click
+        Me.HandlekeyInput("/")
+    End Sub
+
+    Private Sub Key7_Click(sender As Object, e As EventArgs) Handles Key7.Click
+        Me.HandlekeyInput("7")
+    End Sub
+
+    Private Sub Key8_Click(sender As Object, e As EventArgs) Handles Key8.Click
+        Me.HandlekeyInput("8")
+    End Sub
+
+    Private Sub Key9_Click(sender As Object, e As EventArgs) Handles Key9.Click
+        Me.HandlekeyInput("9")
+    End Sub
+
+    Private Sub Key4_Click(sender As Object, e As EventArgs) Handles Key4.Click
+        Me.HandlekeyInput("4")
+    End Sub
+
+    Private Sub Key5_Click(sender As Object, e As EventArgs) Handles Key5.Click
+        Me.HandlekeyInput("5")
+    End Sub
+
+    Private Sub Key6_Click(sender As Object, e As EventArgs) Handles Key6.Click
+        Me.HandlekeyInput("6")
+    End Sub
+
+    Private Sub Key1_Click(sender As Object, e As EventArgs) Handles Key1.Click
+        Me.HandlekeyInput("1")
+    End Sub
+
+    Private Sub Key2_Click(sender As Object, e As EventArgs) Handles Key2.Click
+        Me.HandlekeyInput("2")
+    End Sub
+
+    Private Sub Key3_Click(sender As Object, e As EventArgs) Handles Key3.Click
+        Me.HandlekeyInput("3")
+    End Sub
+
+    Private Sub Key0_Click(sender As Object, e As EventArgs) Handles Key0.Click
+        Me.HandlekeyInput("0")
+    End Sub
+
+    Private Sub KeyComma_Click_1(sender As Object, e As EventArgs) Handles KeyComma.Click
+        Me.HandlekeyInput(",")
+    End Sub
+
+    Private Sub KeyReverse_Click(sender As Object, e As EventArgs) Handles KeyReverse.Click
+        Dim expr = Me.ResultField.Text
+
+        If Regex.IsMatch(expr, "^(\d+[,]?\d*){1}$") Then
+            expr = Me.AddLessSign(expr)
+        ElseIf Regex.IsMatch(expr, "^-(\d+[,]?\d*){1}$") Then
+            expr = Me.DeleteLessSign(expr)
+        End If
+
+        Me.ResetTextBox()
+        Me.DisplayOnTextBox(expr)
+    End Sub
+
+    Private Sub KeyCalc_Click(sender As Object, e As EventArgs) Handles KeyCalc.Click
+        Dim expr = Me.ResultField.Text
+        Dim result As Double
+
+        If String.IsNullOrEmpty(expr) Then
+            Exit Sub
+        End If
+
+        If IsSinglePercent(expr) Then
+            expr = "0"
+        End If
+
+
+        expr = Me.ReplacePercentSignByCalculable(expr)
+        expr = Me.ReplaceCommasByDots(expr)
+        expr = Me.ReplaceXByStarChar(expr)
+
+
+        result = Calculate(expr)
+        Me.ResetTextBox()
+        Me.DisplayOnTextBox(result)
+
+    End Sub
+
+    Private Sub KeyPlus_Click(sender As Object, e As EventArgs) Handles KeyPlus.Click
+        Me.HandlekeyInput("+")
+    End Sub
+
+    Private Sub KeyLess_Click(sender As Object, e As EventArgs) Handles KeyLess.Click
+        Me.HandlekeyInput("-")
+    End Sub
+
+    Private Sub KeyMul_Click(sender As Object, e As EventArgs) Handles KeyMul.Click
+        Me.HandlekeyInput("x")
+    End Sub
+    Private Sub HandlekeyInput(input As String)
+
+        Dim concatText = Me.ResultField.Text & input
+
+
+        If IsEquationWithOptions(concatText) Then
+            Me.DisplayOnTextBox(concatText)
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Function Calculate(expression As String) As Double
+        Dim result = New DataTable().Compute(expression, Nothing)
+        Calculate = result
+    End Function
+
+    Private Sub DisplayOnTextBox(input As String)
+        Me.ResultField.Text = input
+    End Sub
+
+    Private Sub ResetTextBox()
+        Me.ResultField.ResetText()
+    End Sub
+
+    Private Function IsEquationWithOptions(text As String)
+        IsEquationWithOptions = Regex.IsMatch(text, "^[-]?(\d+[,]?\d*)?[-+\/x]?[-]?(\d+[,]?\d*[%]?)?$")
+    End Function
+
+
+    Private Function IsSinglePercent(Text As String)
+        IsSinglePercent = Regex.IsMatch(Text, "^[-]?(\d+[,]?\d*){1}%$")
+    End Function
+
+
+    Private Function ReplaceCommasByDots(text As String)
+
+        If Regex.IsMatch(text, ",") Then
+            ReplaceCommasByDots = Regex.Replace(text, ",", ".")
+            Exit Function
+        End If
+
+        ReplaceCommasByDots = text
+    End Function
+
+
+    Private Function ReplaceXByStarChar(text As String)
+        If Regex.IsMatch(text, "x") Then
+            ReplaceXByStarChar = Regex.Replace(text, "x", "*")
+            Exit Function
+        End If
+
+        ReplaceXByStarChar = text
+    End Function
+
+    Private Function ReplacePercentSignByCalculable(text As String)
+        Dim exprMatch As Match
+        Dim persNumber As Double
+        Dim leftNumber As String
+        Dim res As String
+
+        If Regex.IsMatch(text, "%") Then
+            exprMatch = Regex.Match(text, "^[-]?(\d+[,]?\d*)?[-+\/x]?[-]?(\d+[,]?\d*)([%]{1})$")
+
+            If exprMatch.Success Then
+                persNumber = CDbl(exprMatch.Groups(2).Value)
+                leftNumber = exprMatch.Groups(1).Value
+                res = CStr(leftNumber) & "*" & CStr(persNumber / 100)
+                res = Regex.Replace(text, "(\d+[,]?\d*)([%]{1})$", res)
+                ReplacePercentSignByCalculable = res
+                Exit Function
+            End If
+        End If
+
+        ReplacePercentSignByCalculable = text
+    End Function
+
+
+    Private Function AddLessSign(text As String)
+        AddLessSign = Regex.Replace(text, "(\d+[,]?\d*)", "-$1")
+    End Function
+
+
+    Private Function DeleteLessSign(text As String)
+        DeleteLessSign = Regex.Replace(text, "-(\d+[,]?\d*)", "$1")
+    End Function
+
+End Class
